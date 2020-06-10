febdf8594431482a4a26bbec2c79877c2197b61d
iantheninja <munene.ian@live.com>
2020-06-10 21:36:42 +0300

    business logic for bank and accounts complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,34 @@
+// Business logic for bank
+function Bank() {
+    this.accounts = [];
+    this.currentId = 0;
+}
+
+Bank.prototype.addAccount = function(account) {
+    account.id = this.assignId();
+    this.accounts.push(account);
+}
+
+Bank.prototype.assignId = function(account) {
+    return this.currentId += 1;
+}
+
+
+// Business logic for accounts
+function Account(name, initialDeposit) {
+    this.name = name;
+    this.initialDeposit = initialDeposit;
+}
+
+Account.prototype.withdraw = function(amount) {
+    return this.initialDeposit - amount;
+}
+
+Account.prototype.deposit = function(amount) {
+    return this.initialDeposit + amount;
+}
+
+// UI logic for accounts
 $(document).ready(function() {
-    console.log("This page is ready");
+    
 })
